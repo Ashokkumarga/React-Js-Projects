56ffcbbf949495040c78c8c050b3ac2567c1ad90
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-07 12:10:51 +0530

    com2

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Home from "./routes/Home";
+import Project from "./routes/Project";
+import Routes from "./react-router-dom";
+import Route from "./react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>PortFolio Website</h1>
-      <h2>hey</h2>
-    </div>
+    <>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Project" element={<Project />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
