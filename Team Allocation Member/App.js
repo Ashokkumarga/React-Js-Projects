916909bf812cdb448a35efd2e164e513974e199e
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-12 12:44:39 +0530

    Team Allocation App

---
+++ Team Allocation Member/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import App1 from "./TeamAllocationWebsite/App1";
+// import PortfolioWeb from "./PortfolioWebSite/PortfolioWeb";
+
+function App() {
+  return (
+    <>
+      {/* <PortfolioWeb /> */}
+      <App1 />
+    </>
+  );
+}
+
+export default App;
