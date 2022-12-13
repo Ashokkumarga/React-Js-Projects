bd6ab5be2db2d4bc567edea133499a817e4cd10d
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-13 19:58:26 +0530

    EOD

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 // import Message from "./Message";
-// import App1 from "./TeamAllocationWebsite/App1";
-import PortfolioWeb from "./PortfolioWebSite/PortfolioWeb";
+import App1 from "./TeamAllocationWebsite/App1";
+// import PortfolioWeb from "./PortfolioWebSite/PortfolioWeb";
 
 function App() {
   return (
     <>
-      <PortfolioWeb />
-      {/* <App1 /> */}
+      {/* <PortfolioWeb /> */}
+      <App1 />
       {/* <Message /> */}
     </>
   );
