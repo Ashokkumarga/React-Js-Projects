03b9e9e91fa984f1448e61f365d6b2a9a3c16b2c
ashokkumarga <ashokkumarga0@gmail.com>
2022-12-15 20:01:22 +0530

    Contact App

--- portfolio/src/Contact App/Appc.js
+++ portfolio/src/Contact App/Appc.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { uuid } from "uuidv4";
 import Header from "./Components/Header";
-import ContactCard from "./Components/ContactCard";
 import ContactList from "./Components/ContactList";
 import AddContact from "./Components/AddContact";
 const Appc = () => {
-    const contacts =[
-        {
-            id:1,
-            "name": "Ashok",
-            "email" : "sunnyleonefans@association.com"
-        }
-    ]
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setcontacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setcontacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setcontacts(newContactList);
+  };
+  useEffect(() => {
+    const retirveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retirveContacts) setcontacts(retirveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
   return (
     <div className="ui container">
       <Header />
-      <ContactCard />
-      <AddContact />
-      <ContactList contacts={contacts} />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 };
